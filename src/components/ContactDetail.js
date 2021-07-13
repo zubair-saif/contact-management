ed94c668023353743934d22737074c30374bcdf5
zubair-saif <zubairsaif700@gmail.com>
2021-07-13 14:01:35 +0500

    ..

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -2,18 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function ContactDetails(props) {
-  const { id, name, email } = props.location.state.contact;
+  const { name, email } = props.location.state.contact;
   return (
     <div>
       <div className="card">
         <img src="..." className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
-          <p className="card-text">
-            {email}
-          </p>
-          <Link to='/'><button  className="btn btn-primary">Go back to Contact List</button></Link>
-          
+          <p className="card-text">{email}</p>
+          <Link to="/">
+            <button className="btn btn-primary">Go back to Contact List</button>
+          </Link>
         </div>
       </div>
     </div>
